0b459191f66e7892fb7fae0a3ff48c6a401518af
AyloSrd <aylo.srd@gmail.com>
2021-01-29 15:14:57 +0100

    started skill tag adding

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -1,7 +1,8 @@
 export const skills = {
 	categories: ['Computer Skills', 'Business Skills', 'Soft Skills'],
-	computerSkills: ['javascript', 'html', 'css', 'react', 'svelte', 'git', 'github', 'heroku', 'vercel', 'aws', 'docker', 'node.js', 'deno', 'mongoDB', 'NoSQL', 'socket.io', 'peerjs', 'webRTC'],
-	businessSkills: ['sales', 'marketing']
+	computerSkills: ['aws', 'css', 'deno', 'docker', 'face-api.js', 'git', 'github', 'heroku', 'html', 'javascript', 'mongoDB', 'noSQL', 'node.js', 'peerjs', 'react', 'socket.io', 'svelte', 'tensorflow.js', 'vercel', 'webRTC'],
+	businessSkills: ['sales', 'marketing', 'Google Ads', 'Google Analytics'],
+	softSkills: ['teamworking', 'negotiation', 'storytelling', 'sleight-of-hand', 'stubborness', 'creativity', 'stress management', 'adaptability', 'problem-solving']
 }
 
 export const portfolio = [
---
+++ src/modules/templates/SkillTag.js
@@ -0,0 +1,16 @@
+const SkillTag = { skill, researchedSkill } => {
+	const template = `
+	<span
+		class="skill-tag${ 
+			researchedSkill 
+			? 'researched-skill'
+			: ''
+		}"
+	>
+		${skill}
+	</span>
+	`
+	return template
+}
+
+export default SkillTag
