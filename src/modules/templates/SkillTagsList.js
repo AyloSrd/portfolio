651ba6ccf1b269a65efc4140330736cd4c89f607
AyloSrd <aylo.srd@gmail.com>
2021-03-03 15:41:45 +0100

    aaddressed skill tag section horizontal scroll on mobile issue

--- src/modules/templates/SkillTagsList.js
+++ src/modules/templates/SkillTagsList.js
@@ -2,15 +2,17 @@ import SkillTag from './SkillTag.js'
 
 const SkillTagsList = ({ title, skills }) => {
 	const template =  `
-	${ title
-		? `<h3>${ title }</h3>` 
-		: ''
-	}
-	<div class="skill-list">
-		${
-			skills.map(skill => SkillTag({ skill, researchedSkill: title ? false : true})).join('\n')
-		}
 	<div>
+		${ title
+			? `<h3>${ title }</h3>` 
+			: ''
+		}
+		<div class="skill-list">
+			${
+				skills.map(skill => SkillTag({ skill, researchedSkill: title ? false : true})).join('\n')
+			}
+		</div>
+	</div>
 	`
 	return template
 }
